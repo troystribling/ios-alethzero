d0be9a761d1b198de5a3bcc05ab175f104c84bc2
Troy Stribling <troy.stribling@gmail.com>
2014-04-29 06:49:44 -0700

    getting started with UI

---
+++ ios-alethzero/AlethZeroNetworkTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlethZeroNetworkTableViewController.h
+//  ios-alethzero
+//
+//  Created by Troy Stribling on 4/28/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlethZeroNetworkTableViewController : UITableViewController
+
+@end
---
+++ ios-alethzero/AlethZeroNetworkTableViewController.m
@@ -0,0 +1,34 @@
+//
+//  AlethZeroNetworkTableViewController.m
+//  ios-alethzero
+//
+//  Created by Troy Stribling on 4/28/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import "AlethZeroNetworkTableViewController.h"
+
+@interface AlethZeroNetworkTableViewController ()
+
+@end
+
+@implementation AlethZeroNetworkTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+@end
