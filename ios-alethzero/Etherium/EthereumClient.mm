86753474644e29b39245b67499eda46f4273d09c
Troy Stribling <troy.stribling@gmail.com>
2014-05-08 06:55:43 -0700

    working through network connection

--- ios-alethzero/Etherium/EthereumClient.mm
+++ ios-alethzero/Etherium/EthereumClient.mm
@@ -8,9 +8,15 @@
 #import "EthereumClient.h"
 #import <ethereum/Client.h>
 
+using namespace std;
+using namespace eth;
+
 @interface EthereumClient () {
     std::unique_ptr<eth::Client>    client;
 }
+
+- (void)startNetwork;
+- (void)setClientVersion;
 
 @end
 
@@ -23,15 +29,27 @@
 - (id)initWithName:(NSString*)a_name {
     self = [super init];
     if (self) {
-        client.reset(new eth::Client(std::string([a_name UTF8String])));
+        client.reset(new Client(std::string([a_name UTF8String])));
     }
     return self;
 }
 
 - (void)connectToPeerAddress:(NSString*)a_address atPort:(NSString*)a_port {
     uint16_t port = (uint16_t)[a_port integerValue];
-    std::string address([a_address UTF8String]);
+    string address([a_address UTF8String]);
     client->connect(address, port);
+}
+
+#pragma mark - Private -
+
+- (void)startNetwork {
+}
+
+- (void)setClientVersion {
+//    string n = "iOS-AlethZero/v" ETH_QUOTED(ETH_VERSION);
+//	if (ui->clientName->text().size())
+//		n += "/" + ui->clientName->text().toStdString();
+//	n +=  "/" ETH_QUOTED(ETH_BUILD_TYPE) "/" ETH_QUOTED(ETH_BUILD_PLATFORM);
 }
 
 @end
